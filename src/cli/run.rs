f8e7a3f97cfd605728d57d462160a641ae4980ed
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-04-27 16:23:42 +0200

    cli: Add a bit of user-facing logging

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -75,6 +75,8 @@ pub fn run_1d<P>(path: P, args: &Cli) -> clap::Result<()>
 where
     P: AsRef<Path>,
 {
+    log::info!(target: "init", "Reading SAF file into memory:\n\t{}", path.as_ref().display());
+
     let reader = saf::Reader::from_bgzf_member_path(path)?;
 
     let mut saf = Saf1d::read(reader)?;
@@ -90,8 +92,14 @@ pub fn run_2d<P>(first_path: P, second_path: P, args: &Cli) -> clap::Result<()>
 where
     P: AsRef<Path>,
 {
-    let first_reader = saf::Reader::from_bgzf_member_path(first_path)?;
-    let second_reader = saf::Reader::from_bgzf_member_path(second_path)?;
+    log::info!(
+        target: "init",
+        "Reading and intersecting SAF files into memory:\n\t{}\n\t{}",
+        first_path.as_ref().display(), second_path.as_ref().display()
+    );
+
+    let first_reader = saf::Reader::from_bgzf_member_path(&first_path)?;
+    let second_reader = saf::Reader::from_bgzf_member_path(&second_path)?;
 
     let mut safs = Saf2d::read(first_reader, second_reader)?;
     let sites = safs.sites();
@@ -119,6 +127,12 @@ pub fn run_io<P>(path: P, args: &Cli) -> clap::Result<()>
 where
     P: AsRef<Path>,
 {
+    log::info!(
+        target: "init",
+        "Streaming pseudo-shuffled SAF file:\n\t{}",
+        path.as_ref().display()
+    );
+
     let mut reader = crate::io::Reader::from_path(path)?;
     let header = reader.read_header()?;
 
@@ -128,6 +142,12 @@ where
         .map(|x| (x + 1) as usize)
         .collect::<Vec<_>>();
     let sites = header.sites() as usize;
+
+    log::info!(
+        target: "init",
+        "Streaming {sites} sites in pseudo-shuffled SAF file with {} cols and {} blocks.",
+        alleles.iter().map(|x| x.to_string()).collect::<Vec<_>>().join("/"), header.blocks()
+    );
 
     match alleles[..] {
         [n] => run_io!(reader, header, args, [n], sites),
