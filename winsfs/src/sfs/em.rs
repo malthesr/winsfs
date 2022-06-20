3f6d21903ecf8bb49d087241561b31f7ccf1e821
Malthe Sebro Rasmussen <malthesebro@protonmail.com>
2022-06-20 16:07:10 +0200

    sfs/em: Update functions to return typed likelihood types
    Prevents bugs, also uses SumOf to allow normalisation over input
    size downstream where desired.

--- winsfs/src/sfs/em.rs
+++ winsfs/src/sfs/em.rs
@@ -1,18 +1,20 @@
 use std::io;
 
-use rayon::iter::ParallelIterator;
+use rayon::iter::{IndexedParallelIterator, ParallelIterator};
 
 use crate::{
+    em::likelihood::{Likelihood, LogLikelihood, SumOf},
     io::ReadSite,
     saf::{
         iter::{IntoParallelSiteIterator, IntoSiteIterator},
         AsSiteView, Site,
     },
-    Sfs, UnnormalisedSfs,
 };
 
+use super::{Sfs, UnnormalisedSfs};
+
 impl<const N: usize> Sfs<N> {
-    /// Returns the log-likelihood of the SFS given the input, and the expected number of sites
+    /// Returns the log-likelihood of the data given the SFS, and the expected number of sites
     /// in each frequency bin given the SFS and the input.
     ///
     /// This corresponds to an E-step for the EM algorithm. The returned SFS corresponds to the
@@ -38,23 +40,30 @@ impl<const N: usize> Sfs<N> {
     /// assert_eq!(posterior, sfs1d![2., 1., 0., 1., 0.]);
     /// assert_eq!(log_likelihood, sfs.log_likelihood(&saf));
     /// ```
-    pub fn e_step<I>(&self, input: I) -> (f64, UnnormalisedSfs<N>)
+    pub fn e_step<I>(&self, input: I) -> (SumOf<LogLikelihood>, UnnormalisedSfs<N>)
     where
         I: IntoSiteIterator<N>,
     {
-        let (ll, post, _) = input.into_site_iter().fold(
-            (0.0, Sfs::zeros(self.shape()), Sfs::zeros(self.shape())),
-            |(mut ll, mut post, mut buf), site| {
-                ll += self.posterior_into(site, &mut post, &mut buf).ln();
+        let iter = input.into_site_iter();
+        let sites = iter.len();
 
-                (ll, post, buf)
+        let (log_likelihood, posterior, _) = iter.fold(
+            (
+                LogLikelihood::from(0.0),
+                Sfs::zeros(self.shape()),
+                Sfs::zeros(self.shape()),
+            ),
+            |(mut log_likelihood, mut posterior, mut buf), site| {
+                log_likelihood += self.posterior_into(site, &mut posterior, &mut buf).ln();
+
+                (log_likelihood, posterior, buf)
             },
         );
 
-        (ll, post)
+        (SumOf::new(log_likelihood, sites), posterior)
     }
 
-    /// Returns the log-likelihood of the SFS given the input, and the expected number of sites
+    /// Returns the log-likelihood of the data given the SFS, and the expected number of sites
     /// in each frequency bin given the SFS and the input.
     ///
     /// This is the parallel version of [`Sfs::e_step`], see also its documentation for more.
@@ -78,28 +87,38 @@ impl<const N: usize> Sfs<N> {
     /// assert_eq!(posterior, sfs1d![2., 1., 0., 1., 0.]);
     /// assert_eq!(log_likelihood, sfs.log_likelihood(&saf));
     /// ```
-    pub fn par_e_step<I>(&self, input: I) -> (f64, UnnormalisedSfs<N>)
+    pub fn par_e_step<I>(&self, input: I) -> (SumOf<LogLikelihood>, UnnormalisedSfs<N>)
     where
         I: IntoParallelSiteIterator<N>,
     {
-        input
-            .into_par_site_iter()
-            .fold(
-                || (0.0, Sfs::zeros(self.shape()), Sfs::zeros(self.shape())),
-                |(mut ll, mut post, mut buf), site| {
-                    ll += self.posterior_into(site, &mut post, &mut buf).ln();
+        let iter = input.into_par_site_iter();
+        let sites = iter.len();
 
-                    (ll, post, buf)
+        let (log_likelihood, posterior) = iter
+            .fold(
+                || {
+                    (
+                        LogLikelihood::from(0.0),
+                        Sfs::zeros(self.shape()),
+                        Sfs::zeros(self.shape()),
+                    )
+                },
+                |(mut log_likelihood, mut posterior, mut buf), site| {
+                    log_likelihood += self.posterior_into(site, &mut posterior, &mut buf).ln();
+
+                    (log_likelihood, posterior, buf)
                 },
             )
-            .map(|(ll, post, _buf)| (ll, post))
+            .map(|(log_likelihood, posterior, _buf)| (log_likelihood, posterior))
             .reduce(
-                || (0.0, Sfs::zeros(self.shape())),
+                || (LogLikelihood::from(0.0), Sfs::zeros(self.shape())),
                 |a, b| (a.0 + b.0, a.1 + b.1),
-            )
+            );
+
+        (SumOf::new(log_likelihood, sites), posterior)
     }
 
-    /// Returns the log-likelihood of the SFS given the input.
+    /// Returns the log-likelihood of the data given the SFS.
     ///
     /// # Panics
     ///
@@ -118,16 +137,21 @@ impl<const N: usize> Sfs<N> {
     /// ];
     /// assert_eq!(sfs.log_likelihood(&saf), 0.2f64.powi(4).ln());
     /// ```
-    pub fn log_likelihood<I>(&self, input: I) -> f64
+    pub fn log_likelihood<I>(&self, input: I) -> SumOf<LogLikelihood>
     where
         I: IntoSiteIterator<N>,
     {
-        input
-            .into_site_iter()
-            .fold(0.0, |ll, site| ll + self.site_log_likelihood(site))
+        let iter = input.into_site_iter();
+        let sites = iter.len();
+
+        let log_likelihood = iter.fold(LogLikelihood::from(0.0), |log_likelihood, site| {
+            log_likelihood + self.site_log_likelihood(site)
+        });
+
+        SumOf::new(log_likelihood, sites)
     }
 
-    /// Returns the log-likelihood of the SFS given the input.
+    /// Returns the log-likelihood of the data given the SFS.
     ///
     /// This is the parallel version of [`Sfs::log_likelihood`].
     ///
@@ -148,14 +172,21 @@ impl<const N: usize> Sfs<N> {
     /// ];
     /// assert_eq!(sfs.par_log_likelihood(&saf), 0.2f64.powi(4).ln());
     /// ```
-    pub fn par_log_likelihood<I>(&self, input: I) -> f64
+    pub fn par_log_likelihood<I>(&self, input: I) -> SumOf<LogLikelihood>
     where
         I: IntoParallelSiteIterator<N>,
     {
-        input
-            .into_par_site_iter()
-            .fold(|| 0.0, |ll, site| ll + self.site_log_likelihood(site))
-            .sum()
+        let iter = input.into_par_site_iter();
+        let sites = iter.len();
+
+        let log_likelihood = iter
+            .fold(
+                || LogLikelihood::from(0.0),
+                |log_likelihood, site| log_likelihood + self.site_log_likelihood(site),
+            )
+            .sum();
+
+        SumOf::new(log_likelihood, sites)
     }
 
     /// Adds the posterior counts for `site` into the provided `posterior buffer`, using the
@@ -165,13 +196,13 @@ impl<const N: usize> Sfs<N> {
     /// will be matched against the shape of `self`, and a panic will be thrown if they do not
     /// match. The shapes of `posterior` and `buf` are unchecked, but must match the shape of self.
     ///
-    /// The likelihood of the site is returned.
-    fn posterior_into<T>(
+    /// The likelihood of the site given the SFS is returned.
+    pub(crate) fn posterior_into<T>(
         &self,
         site: T,
         posterior: &mut UnnormalisedSfs<N>,
         buf: &mut UnnormalisedSfs<N>,
-    ) -> f64
+    ) -> Likelihood
     where
         T: AsSiteView<N>,
     {
@@ -198,10 +229,10 @@ impl<const N: usize> Sfs<N> {
                 *posterior += *buf;
             });
 
-        sum
+        sum.into()
     }
 
-    /// Returns the log-likelihood of the SFS given a single site.
+    /// Returns the log-likelihood of a single site given the SFS.
     ///
     /// # Panics
     ///
@@ -215,15 +246,28 @@ impl<const N: usize> Sfs<N> {
     /// let site = Site::new(vec![1.0, 0.0, 0.0, 0.0, 0.0], [5]).unwrap();
     /// assert_eq!(sfs.site_log_likelihood(site), 0.2f64.ln());
     /// ```
-    pub fn site_log_likelihood<T>(&self, site: T) -> f64
+    pub fn site_log_likelihood<T>(&self, site: T) -> LogLikelihood
     where
         T: AsSiteView<N>,
     {
         self.site_likelihood(site).ln()
     }
 
-    /// Returns the likelihood of the SFS given a single site.
-    fn site_likelihood<T>(&self, site: T) -> f64
+    /// Returns the likelihood of a single site given the SFS.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the shape of the site does not fit the shape of `self`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use winsfs::{saf::Site, Sfs};
+    /// let sfs = Sfs::uniform([5]);
+    /// let site = Site::new(vec![1.0, 0.0, 0.0, 0.0, 0.0], [5]).unwrap();
+    /// assert_eq!(sfs.site_log_likelihood(site), 0.2);
+    /// ```
+    pub fn site_likelihood<T>(&self, site: T) -> Likelihood
     where
         T: AsSiteView<N>,
     {
@@ -240,10 +284,10 @@ impl<const N: usize> Sfs<N> {
             1.,
         );
 
-        sum
+        sum.into()
     }
 
-    /// Returns the log-likelihood of the SFS given the input, and the expected number of sites
+    /// Returns the log-likelihood of the data given the SFS, and the expected number of sites
     /// in each frequency bin given the SFS and the input.
     ///
     /// This is the streaming version of [`Sfs::e_step`], see also its documentation for more.
@@ -251,7 +295,10 @@ impl<const N: usize> Sfs<N> {
     /// # Panics
     ///
     /// Panics if any of the sites in the input does not fit the shape of `self`.
-    pub fn stream_e_step<R>(&self, mut reader: R) -> io::Result<(f64, UnnormalisedSfs<N>)>
+    pub fn stream_e_step<R>(
+        &self,
+        mut reader: R,
+    ) -> io::Result<(SumOf<LogLikelihood>, UnnormalisedSfs<N>)>
     where
         R: ReadSite,
     {
@@ -261,34 +308,40 @@ impl<const N: usize> Sfs<N> {
         let vec = vec![0.0; self.shape().iter().sum()];
         let mut site = Site::new(vec, self.shape()).unwrap();
 
-        let mut ll = 0.0;
+        let mut sites = 0;
+        let mut log_likelihood = LogLikelihood::from(0.0);
         while reader.read_site(site.as_mut_slice())?.is_not_done() {
-            ll += self.posterior_into(&site, &mut post, &mut buf).ln();
+            log_likelihood += self.posterior_into(&site, &mut post, &mut buf).ln();
+
+            sites += 1;
         }
 
-        Ok((ll, post))
+        Ok((SumOf::new(log_likelihood, sites), post))
     }
 
-    /// Returns the log-likelihood of the SFS given the input.
+    /// Returns the log-likelihood of the data given the SFS.
     ///
     /// This is the streaming version of [`Sfs::log_likelihood`].
     ///
     /// # Panics
     ///
     /// Panics if any of the sites in the input does not fit the shape of `self`.
-    pub fn stream_log_likelihood<R>(&self, mut reader: R) -> io::Result<f64>
+    pub fn stream_log_likelihood<R>(&self, mut reader: R) -> io::Result<SumOf<LogLikelihood>>
     where
         R: ReadSite,
     {
         let vec = vec![0.0; self.shape().iter().sum()];
         let mut site = Site::new(vec, self.shape()).unwrap();
 
-        let mut ll = 0.0;
+        let mut sites = 0;
+        let mut log_likelihood = LogLikelihood::from(0.0);
         while reader.read_site(site.as_mut_slice())?.is_not_done() {
-            ll += self.site_log_likelihood(&site);
+            log_likelihood += self.site_log_likelihood(&site);
+
+            sites += 1;
         }
 
-        Ok(ll)
+        Ok(SumOf::new(log_likelihood, sites))
     }
 }
 
@@ -389,7 +442,7 @@ mod tests {
         assert_abs_diff_eq!(posterior.as_slice(), expected.as_slice());
 
         let likelihood = sfs.site_likelihood(site);
-        assert_abs_diff_eq!(likelihood, 2.);
+        assert_abs_diff_eq!(likelihood, Likelihood::from(2.));
         assert_abs_diff_eq!(likelihood, posterior_likelihood);
     }
 
@@ -417,7 +470,7 @@ mod tests {
         assert_abs_diff_eq!(posterior.as_slice(), expected.as_slice(), epsilon = 1e-6);
 
         let likelihood = sfs.site_likelihood(site);
-        assert_abs_diff_eq!(likelihood, 13.);
+        assert_abs_diff_eq!(likelihood, Likelihood::from(13.));
         assert_abs_diff_eq!(likelihood, posterior_likelihood);
     }
 
@@ -445,7 +498,7 @@ mod tests {
         assert_abs_diff_eq!(posterior.as_slice(), expected.as_slice(), epsilon = 1e-5);
 
         let likelihood = sfs.site_likelihood(site);
-        assert_abs_diff_eq!(likelihood, 139.8418, epsilon = 1e-4);
+        assert_abs_diff_eq!(likelihood, Likelihood::from(139.8418), epsilon = 1e-4);
         assert_abs_diff_eq!(likelihood, posterior_likelihood);
     }
 }
